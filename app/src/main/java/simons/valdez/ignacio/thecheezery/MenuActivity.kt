be39c03d0e6a10c23ed48583de61ef5689e8596d
Ignacio Valdez Simons <ignacio.smns@gmail.com>
2021-06-16 18:13:54 -0700

    listviews

--- app/src/main/java/simons/valdez/ignacio/thecheezery/MenuActivity.kt
+++ app/src/main/java/simons/valdez/ignacio/thecheezery/MenuActivity.kt
@@ -1,11 +1,58 @@
 package simons.valdez.ignacio.thecheezery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        var button_coldDrinks:Button = findViewById(R.id.button_coldDrinks) as Button
+        var button_hotDrinks:Button = findViewById(R.id.button_hotDrinks) as Button
+        var button_sweets:Button = findViewById(R.id.button_sweets) as Button
+        var button_salties:Button = findViewById(R.id.button_salties) as Button
+        var button_combos:Button = findViewById(R.id.combos) as Button
+        var button_custom:Button = findViewById(R.id.customizable) as Button
+
+        button_coldDrinks.setOnClickListener{
+            var intent:Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType","coldDrinks")
+            startActivity(intent)
+        }
+
+        button_hotDrinks.setOnClickListener{
+            var intent:Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType","hotDrinks")
+            startActivity(intent)
+        }
+
+        button_sweets.setOnClickListener{
+            var intent:Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType","sweets")
+            startActivity(intent)
+        }
+
+        button_salties.setOnClickListener{
+            var intent:Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType","salties")
+            startActivity(intent)
+        }
+
+        button_combos.setOnClickListener{
+            var intent:Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType","combos")
+            startActivity(intent)
+        }
+
+        button_custom.setOnClickListener{
+            var intent:Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType","custom")
+            startActivity(intent)
+        }
+
+
     }
 }
---
+++ app/src/main/java/simons/valdez/ignacio/thecheezery/Product.kt
@@ -0,0 +1,3 @@
+package simons.valdez.ignacio.thecheezery
+
+data class Product(var name:String="", var image:Int=0, var description:String="", var price:Double=0.0)
